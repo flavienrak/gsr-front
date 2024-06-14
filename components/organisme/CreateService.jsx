559e89cbf13607d0bedf3f55d4e03a7006a5fa89
LionelSocrate <lionelsocrate7@gmail.com>
2024-06-14 15:57:02 +0300

    add navbar

--- components/organisme/CreateService.jsx
+++ components/organisme/CreateService.jsx
@@ -10,6 +10,7 @@ export default function CreateService() {
         <Topbar />
         <div className=" bg-[var(--gray)] w-full h-full flex  gap-8 px-12 py-8 ">
           <Navbar />
+          <div className=" w-full h-full flex flex-col gap-4 px-4 py-6 bg-[var(--white)] "></div>
         </div>
       </div>
     </>
